a9da02402f5771324fdd4bc3a1593b84400e6ca2
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-08 09:28:32 +0530

    [Mukesh] uc1 feature added

--- src/com/game/TicTacToeGame.java
+++ src/com/game/TicTacToeGame.java
@@ -2,8 +2,74 @@ package com.game;
 
 public class TicTacToeGame {
 
-	public static void main(String[] args) {
+	public void uc1() {
 
+	}
+
+	public void uc2() {
+
+	}
+
+	public void uc3() {
+
+	}
+
+	public void uc4() {
+
+	}
+
+	public void uc5() {
+
+	}
+
+	public void uc6() {
+
+	}
+
+	public void uc7() {
+
+	}
+
+	public void uc8() {
+
+	}
+
+	public void uc9() {
+
+	}
+
+	public void uc10() {
+
+	}
+
+	public void uc11() {
+
+	}
+
+	public void uc12() {
+
+	}
+
+	public static void main(String[] args) {
+		
+		TicTacToeGame game = new TicTacToeGame();
+		game.uc1();
+		game.uc2();
+		game.uc3();
+		game.uc4();
+		game.uc5();
+		game.uc6();
+		game.uc7();
+		game.uc8();
+		game.uc9();
+		game.uc10();
+		game.uc11();
+		game.uc12();
+		
+		char[] board = new char[10];
+		for(int i = 1 ; i <=9 ; i++) {
+			board[i] = ' ';
+		}
 	}
 
 }
