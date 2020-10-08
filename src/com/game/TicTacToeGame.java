0f3a719a4839eabaa023085fbff520e2038938fa
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-08 10:34:18 +0530

    [Mukesh] add : uc2 done

--- src/com/game/TicTacToeGame.java
+++ src/com/game/TicTacToeGame.java
@@ -1,19 +1,42 @@
 package com.game;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
+	public static void main(String[] args) {
+
+		char[] board = createBoard();
+		Scanner sc = new Scanner(System.in);
+		char userLetter = selectLetter(sc);
+		char computerLetter = ' ';
+		if (userLetter == 'X') {
+			computerLetter = 'O';
+			System.out.println("Player has chosen " + userLetter + " and Computer has chosen O");
+		} else {
+			computerLetter = 'X';
+			System.out.println("Player has chosen " + userLetter + " and Computer has chosen X");
+		}
+
+	}
+
+	/**
+	 * UC1
+	 */
 	public static char[] createBoard() {
 		char[] board = new char[10];
-		for (int i = 1; i <= 9; i++) {
-			board[i] = ' ';
+		for (int cell = 1; cell <= 9; cell++) {
+			board[cell] = ' ';
 		}
 		return board;
 	}
-	
-	
-	public static void main(String[] args) {
-		
-		char[] board = createBoard();
-	}
 
+	/**
+	 * UC2
+	 */
+	public static char selectLetter(Scanner sc) {
+		System.out.println("Please enter X or O");
+		char select = sc.next().charAt(0);
+		return select;
+	}
 }
